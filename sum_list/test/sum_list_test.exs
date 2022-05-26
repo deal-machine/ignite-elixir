b88d4c13712e3d6c2a40eeae26667dfdcd51d444
deal-machine <95deal@gmail.com>
2022-05-25 21:23:41 -0300

    test: ensure StringHandler turn upper and down case

--- sum_list/test/sum_list_test.exs
+++ sum_list/test/sum_list_test.exs
@@ -1,6 +1,7 @@
 defmodule SumListTest do
   use ExUnit.Case
   doctest SumList
+  doctest StringHandler
 
   test "greets the world" do
     assert SumList.hello() == "world"
@@ -13,5 +14,13 @@ defmodule SumListTest do
   test "is the sum even" do
     total = SumList.sumNumbers(4.0, 0.1)
     assert is_float(total)
+  end
+
+  test "string to downcase" do
+    assert StringHandler.toDownCase("TEST") == "test"
+  end
+
+  test "string to uppercase" do
+    assert StringHandler.toUpperCase("test") == "TEST"
   end
 end
