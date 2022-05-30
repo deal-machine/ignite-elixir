2358bbb6ed9e890eb7aa7af918e7625655d1a8ab
deal-machine <95deal@gmail.com>
2022-05-29 22:05:47 -0300

    refactor: clean return odd counter

--- list_filter/lib/list_filter.ex
+++ list_filter/lib/list_filter.ex
@@ -1,10 +1,5 @@
 defmodule ListFilter do
-
   def call(list) do
-    numbers = Enum.count(list, fn x -> rem(x, 2) != 0 end)
-    count_odd(numbers)
-  end
-  def count_odd(value) do
-    IO.puts(value)
+     Enum.count(list, fn x -> rem(x, 2) != 0 end)
   end
 end
