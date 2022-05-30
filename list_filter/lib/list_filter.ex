004d2278a2cb57a3e6d72591aad73664a34dbe02
deal-machine <95deal@gmail.com>
2022-05-29 22:02:41 -0300

    feat: ensure ListFilter call return odd counter

--- list_filter/lib/list_filter.ex
+++ list_filter/lib/list_filter.ex
@@ -1,18 +1,10 @@
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ListFilter.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def call(list) do
+    numbers = Enum.count(list, fn x -> rem(x, 2) != 0 end)
+    count_odd(numbers)
+  end
+  def count_odd(value) do
+    IO.puts(value)
   end
 end
