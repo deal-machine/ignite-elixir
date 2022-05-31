d9b23080033e5e50e037b3171042a64ac6deca3c
deal-machine <95deal@gmail.com>
2022-05-30 21:20:15 -0300

    test: ensure concat use guard clause

---
+++ sum_list/test/concat_test.exs
@@ -0,0 +1,13 @@
+defmodule ConcatTest do
+  use ExUnit.Case
+
+  test "return a single string" do
+    assert Concat.join("Concatenation test") == "Concatenation test"
+  end
+  test "return two strings" do
+    assert Concat.join("Concat", "strings") == "Concat strings"
+  end
+  test "concatenation with separator" do
+    assert Concat.join("concat two", "strings with", "__") == "concat two__strings with"
+  end
+end
