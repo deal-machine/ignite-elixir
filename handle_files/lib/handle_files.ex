0f40d8b802c258e801f940e669d8439651983081
deal-machine <95deal@gmail.com>
2022-07-18 20:52:49 -0300

    feat: handle file reading errors

--- handle_files/lib/handle_files.ex
+++ handle_files/lib/handle_files.ex
@@ -1,5 +1,16 @@
 defmodule HandleFiles do
   def read(file) do
-    File.read!(file)
+    case File.read(file) do
+      {:ok, content} -> content
+      {:error, error} -> format_error(error)
+    end
+  end
+
+  defp format_error(error) do
+    case error do
+      :enoent -> "Arquivo inexistente"
+      :eacces -> "Arquivo sem permissão"
+      _ -> "Erro desconhecido"
+    end
   end
 end
